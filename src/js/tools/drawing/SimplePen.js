35f7953bb7ef5d7c8613daaa0b35abc829ffc7fb
basimehsan <44620689+basimehsan@users.noreply.github.com>
2022-05-29 22:42:01 +0500

    added smooth brush

--- src/js/tools/drawing/SimplePen.js
+++ src/js/tools/drawing/SimplePen.js
@@ -1,3 +1,50 @@
+function hexToRgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
+
+function hexToHSL(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    r = parseInt(result[1], 16);
+    g = parseInt(result[2], 16);
+    b = parseInt(result[3], 16);
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+    if(max == min){
+      h = s = 0; // achromatic
+    }else{
+      var d = max - min;
+      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+      switch(max){
+        case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+        case g: h = (b - r) / d + 2; break;
+        case b: h = (r - g) / d + 4; break;
+      }
+      h /= 6;
+    }
+  var HSL = new Object();
+  HSL['h']=h;
+  HSL['s']=s;
+  HSL['l']=l;
+  return HSL;
+}
+
+function hslToHex(h, s, l) {
+  l /= 100;
+  const a = s * Math.min(l, 1 - l) / 100;
+  const f = n => {
+    const k = (n + h / 30) % 12;
+    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+    return Math.round(255 * color).toString(16).padStart(2, '0');   // convert to Hex and prefix "0" if needed
+  };
+  return `#${f(0)}${f(8)}${f(4)}`;
+}
+
 /**
  * @provide pskl.tools.drawing.SimplePen
  *
@@ -32,7 +79,25 @@
 
     var color = this.getToolColor();
 
+    rgbColor = hexToRgb(color);
+    hlsColor = hexToHSL(color);
+
+    var lightColor = hslToHex(hlsColor.h * 360, hlsColor.s * 100, Math.min(100, hlsColor.l * 100 + 12.5));
+    var lighterColor = hslToHex(hlsColor.h * 360, hlsColor.s * 100, Math.min(100, hlsColor.l * 100 + 25));
+
     this.drawUsingPenSize(color, col, row, frame, overlay);
+
+    // Sides
+    this.drawUsingPenSize(lightColor, col-1, row, frame, overlay);
+    this.drawUsingPenSize(lightColor, col+1, row, frame, overlay);
+    this.drawUsingPenSize(lightColor, col, row+1, frame, overlay);
+    this.drawUsingPenSize(lightColor, col, row-1, frame, overlay);
+
+    // Diagonals
+    this.drawUsingPenSize(lighterColor, col-1, row-1, frame, overlay);
+    this.drawUsingPenSize(lighterColor, col+1, row+1, frame, overlay);
+    this.drawUsingPenSize(lighterColor, col-1, row+1, frame, overlay);
+    this.drawUsingPenSize(lighterColor, col+1, row-1, frame, overlay);
   };
 
   ns.SimplePen.prototype.drawUsingPenSize = function(color, col, row, frame, overlay) {
